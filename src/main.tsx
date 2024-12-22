d8589af04fb27c8350b270de4b3082f20685daea
David Choloyan <shamsani.david@gmail.com>
2024-12-22 10:26:19 +0100

    feat: tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { createRouter, RouterProvider } from "@tanstack/react-router";
 
 import { routeTree } from "./routeTree.gen";
 
+import "./styles/global.css";
+
 const router = createRouter({ routeTree });
 
 declare module "@tanstack/react-router" {
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });

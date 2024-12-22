d3811c97ff73074ea197304b382794493d7d9a9e
David Choloyan <shamsani.david@gmail.com>
2024-12-22 10:33:56 +0100

    feat: handle fetch

---
+++ src/constants/api.ts
@@ -0,0 +1,2 @@
+export const API_KEY = "1b9a8df51e30fd57ec89c6a158b307df";
+export const baseUrl = "https://api.openweathermap.org/geo/1.0/direct";
---
+++ src/lib/handleFetch.ts
@@ -0,0 +1,72 @@
+import type { StringifiableRecord } from "query-string";
+
+import { baseUrl } from "../constants/api";
+
+interface FetchError extends Error {
+  status?: number;
+}
+
+interface FetchOptions extends RequestInit {
+  params?: StringifiableRecord;
+  timeout?: number;
+  retryCount?: number;
+  headers?: HeadersInit;
+  body?: BodyInit;
+}
+
+const fetchWithTimeout = (url: string, timeout: number, options?: RequestInit): Promise<Response> => {
+  return new Promise((resolve, reject) => {
+    const timer = setTimeout(() => reject(new Error("Request timed out")), timeout);
+    fetch(url, { ...options })
+      .then((response) => {
+        clearTimeout(timer);
+        resolve(response);
+      })
+      .catch((error) => {
+        clearTimeout(timer);
+        reject(error);
+      });
+  });
+};
+
+export const handleFetch = async <T>(options?: FetchOptions): Promise<T> => {
+  const timeout = options?.timeout ?? 30000;
+  const retryCount = options?.retryCount ?? 2;
+  const headers = options?.headers;
+  const body = options?.body;
+  const method = options?.method ?? "GET";
+
+  for (let attempt = 0; attempt <= retryCount; attempt++) {
+    try {
+      const response = await fetchWithTimeout(baseUrl, timeout, {
+        method,
+        headers,
+        body,
+      });
+
+      if (response.status !== 200) {
+        const error: FetchError = new Error("The network response failed.");
+        error.status = response.status;
+        throw error;
+      }
+
+      const data: T = await response.json();
+
+      return { ...data };
+    } catch (error) {
+      if (attempt !== retryCount) {
+        console.warn(`Attempt ${attempt + 1} failed. Retrying...`);
+        continue;
+      }
+
+      if (!(error instanceof Error)) {
+        throw new Error("An unknown error occurred during fetch");
+      }
+
+      console.error("Fetch error:", error.message);
+      throw error;
+    }
+  }
+
+  throw new Error("Failed to fetch after retries");
+};
